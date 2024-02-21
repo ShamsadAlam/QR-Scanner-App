ffede661bbb84311a053dc01da4dfd6c61cf5879
ShamsadAlam <nobleperson7571@gmail.com>
2024-02-21 15:02:09 +0530

    repository intialized

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "@firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdLNTyItEb17fVDy7LILUiCu0x1X7EjSw",
+  authDomain: "qr-code-scanner-4b367.firebaseapp.com",
+  projectId: "qr-code-scanner-4b367",
+  storageBucket: "qr-code-scanner-4b367.appspot.com",
+  messagingSenderId: "333384106453",
+  appId: "1:333384106453:web:2d0c467a6113d8be24bd5c",
+  measurementId: "G-JK9J2KNZRR",
+};
+
+const app = initializeApp(firebaseConfig);
